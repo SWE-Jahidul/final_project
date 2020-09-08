06b1707883de49e5c5ef11267326cd9f66e80858
SWE-Jahidul <programmerjahid162@gmail.com>
2020-09-08 23:13:28 +0600

    first commit

---
+++ APP/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path 
+
+
+from . import views 
+
+urlpatterns = [
+    path('' , views.home , name ='home'),
+    path('login/', views.login , name = 'login'),
+    path('registration/', views.registration , name ='registration'),
+    path('complain/', views.complain , name ='complain' ),
+    path('mayor_and_councilor/' , views.mayor_and_councilor , name ='mayor_and_councilor'),
+    path('notice/' , views.notice , name ='notice'),
+
+]
---
+++ APP/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+
+
+
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def registration(request):
+        return render(request, 'registration.html')
+
+
+def complain(request):
+    return render(request,'compalin.html')
+
+
+
+def mayor_and_councilor(request):
+    return render(request, 'mayor_and_councilor.html')
+
+
+def notice(request):
+    return render(request, 'notices.html')
