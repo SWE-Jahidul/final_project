5555e1ad038706ac8ade91be1d39ccf32ad3a53f
SWE-Jahidul <programmerjahid162@gmail.com>
2020-09-09 23:45:37 +0600

    adding model

--- APP/admin.py
+++ APP/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Notice,Mayor_and_councilor,News,Contract
+
+# Register your models here.
+admin.site.register(Notice)
+admin.site.register(Mayor_and_councilor)
+admin.site.register(News)
+admin.site.register(Contract)
--- APP/models.py
+++ APP/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class Notice(models.Model):
+    title = models.CharField(max_length=200,default='') 
+    heading = models.CharField(max_length=150,default='') 
+    Notice_decription =  models.TextField(max_length=1024*3, default='')
+    notice_date = models.DateTimeField(auto_now_add=True ) 
+    
+    def __str__(self):
+        return self.title
+
+
+
+class Mayor_and_councilor(models.Model):
+    mayor_or_councilor_name = models.CharField(max_length=150,default='') 
+    deginaion = models.CharField(max_length=100,default='') 
+    email = models.CharField(max_length=150,default='') 
+    about = models.CharField(max_length=250,default='')
+
+    def __str__(self):
+        return self.mayor_or_councilor_name
+
+
+
+
+
+class News(models.Model):
+    news_title = models.CharField(max_length=150, default='') 
+    news_details = models.CharField(max_length=1024*3 , default='')
+    news_date = models.DateTimeField(auto_now_add=True )
+
+    def __str__(self):
+            return self.news_title
+
+    
+    
+
+class Contract(models.Model):
+    contact_number_title =  models.CharField(max_length=150,default='')
+    phone_number = models.IntegerField()
+
+    def __str__(self):
+        return self.contact_number_title
+
+
